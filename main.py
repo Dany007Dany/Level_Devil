0077299ffeccf255b0db017d9779969b607a6ce0
Dany007Dany <ligadany0@gmail.com>
2024-06-06 09:08:17 +0200

    Immagine start, throw e kunai

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import pygame
 import sys
 import math
-from player import Player
+from player import Player, Kunai
 pygame.init()
+
 #CLOCK
 clock = pygame.time.Clock()
+
 #WINDOW SETTINGS
 WIDTH = 64*20
 HEIGHT  = 64*10
@@ -20,58 +22,90 @@ ground1 = pygame.image.load("Map Images/Tiles/2.png").convert_alpha()
 ground1 = pygame.transform.scale(ground1, (64, 64))
 ground1_width = ground1.get_width()
 tiles_2 = math.ceil(WIDTH / ground1_width + 1)
-
 #VARIABLES FOR SCROLLING EFFECT
-key = pygame.key.get_pressed()
+keys = pygame.key.get_pressed()
 scroll = 0
 scroll_2 = 0
 tiles = math.ceil(WIDTH / background_width) + 1
 
-
 #PLAYER AND BOTS
-moving_sprites_player_idle = pygame.sprite.Group()
+moving_sprites_player = pygame.sprite.Group()
 player = Player(200, (HEIGHT - 155))
-moving_sprites_player_idle.add(player)
+moving_sprites_player.add(player)
+#KUNAI
+kunai = Kunai(HEIGHT)
 
 
 #STARTING DISPLAY
+game_active = False
+wallpaper_start = pygame.image.load("Ninja Images/Sfondo_Start.jpeg")
+wallpaper_start = pygame.transform.scale(wallpaper_start, (WIDTH, HEIGHT))
+font = pygame.font.Font()
 
 
-
+#LOST DISPLAY
+#WIN DISPLAY
 
 #MAIN LOOP
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit
+            pygame.quit()
             sys.exit()
 
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
+            if event.key == pygame.K_SPACE or event.key == pygame.K_w:
                 player.jump()
-
-    player.animate()
-    player.apply_gravity()
-
-    #SCROLLING BACKGROUND
-    for i in range(0, tiles):
-        screen.blit(background, (i * background_width + scroll,0))
-    for i in range(0, tiles_2):
-        screen.blit(ground1, (i * ground1_width + scroll_2, HEIGHT - 64))
-    
-    scroll -= 5
-    scroll_2 -= 5
-    #reset scroll
-    if abs(scroll) > background_width:
-        scroll = 0
-    if abs(scroll_2) > ground1_width:
-        scroll_2 = 0
+            if event.key == pygame.K_k:
+                player.attack()
+            if event.key == pygame.K_t:
+                if player.is_throwing == False:
+                    player.throw_kunai()
+            if event.key == pygame.K_t and kunai.is_flying == False:    
+                kunai.is_flying = True
+            if event.key == pygame.K_p:
+                game_active = True
+        
     
 
-    #PLAYER BLIT AND FUNCTIONS
-    moving_sprites_player_idle.draw(screen)
-    
-    player.update()
+    if game_active == True:
+        player.animate()
+        player.apply_gravity()
+
+        #SCROLLING BACKGROUND
+        for i in range(0, tiles):
+            screen.blit(background, (i * background_width + scroll,0))
+        for i in range(0, tiles_2):
+            screen.blit(ground1, (i * ground1_width + scroll_2, HEIGHT - 64))
+        
+        scroll -= 5
+        scroll_2 -= 5
+        #reset scroll
+        if abs(scroll) > background_width:
+            scroll = 0
+        if abs(scroll_2) > ground1_width:
+            scroll_2 = 0
+        
+
+        #PLAYER BLIT AND FUNCTIONS
+        moving_sprites_player.draw(screen)
+        if kunai.is_flying:
+            kunai.fly()
+            kunai.draw(screen)
+            # Reset kunai if it goes off screen or hits a certain condition
+            if kunai.kunai_rect.x > WIDTH or kunai.kunai_rect.y > HEIGHT:
+                kunai.reset()
+            
+        
+        player.update()
+
+    elif game_active == False:
+        screen.blit(wallpaper_start, (0, 0))
+        moving_sprites_player.draw(screen)
+        player.animate()
+        player.init_update(WIDTH, HEIGHT)
+        
+
     #SCREEN UPDATE
     pygame.display.update()
     pygame.display.flip()
--- player.py
+++ player.py
@@ -7,34 +7,46 @@ class Player(pygame.sprite.Sprite):
         self.pos_x = pos_x
         self.pos_y = pos_y
 #RUN STATE OF PLAYER
-        self.sprites_run = [pygame.image.load(f"Ninja Images/Run__00{i}.png") for i in range(10)]
+        self.sprites_run = [pygame.image.load(f"Ninja Images/Run__00{i}.png").convert_alpha() for i in range(10)]
         self.current_run = 0
         self.image = self.sprites_run[self.current_run]
         self.image = pygame.transform.scale(self.image, (75, 100))
         self.rect = self.image.get_rect(topleft = (pos_x, pos_y))
 #IDLE STATE OF PLAYER
-        self.sprites_idle = [pygame.image.load(f"Ninja Images/Idle__00{i}.png") for i in range(10)]
+        self.sprites_idle = [pygame.image.load(f"Ninja Images/Idle__00{i}.png").convert_alpha() for i in range(10)]
         self.current_idle = 0
         self.image = self.sprites_idle[self.current_idle]
         self.image = pygame.transform.scale(self.image, (50, 100))
         self.rect = self.image.get_rect(topleft = (pos_x, pos_y))
 #JUMP STATE OF PLAYER
-        self.sprites_jump = [pygame.image.load(f"Ninja Images/Jump__00{i}.png") for i in range(10)]
+        self.sprites_jump = [pygame.image.load(f"Ninja Images/Jump__00{i}.png").convert_alpha() for i in range(10)]
         self.current_jump = 0
         self.image = self.sprites_jump[self.current_jump]
         self.image = pygame.transform.scale(self.image, (75,100))
         self.rect = self.image.get_rect(topleft = (pos_x, pos_y))
-
-
+#ATTACK STATE OF PLAYER
+        self.sprites_attack = [pygame.image.load(f"Ninja Images/Attack__00{i}.png").convert_alpha() for i in range(10)]
+        self.current_attack = 0
+        self.image = self.sprites_attack[self.current_attack]
+        self.image = pygame.transform.scale(self.image, (100, 100))
+        self.rect = self.image.get_rect(topleft = (pos_x, pos_y))
+#THROWING STATE OF PLAYER
+        self.sprites_throwing = [pygame.image.load(f"Ninja Images/Throw__00{i}.png").convert_alpha() for i in range (10)]
+        self.current_throwing = 0
+        self.image = self.sprites_throwing[self.current_throwing]
+        self.image = pygame.transform.scale(self.image, (80, 100))
+        self.rect = self.image.get_rect(topleft = (pos_x, pos_y))
 
 #OTHER VARIABLES
         self.gravity_force = 0
         self.is_animating = False
         self.is_jumping = False
-        
+        self.is_attacking = False
+        self.is_throwing = False
     
     def animate(self):
          self.is_animating = True
+
     def not_animate(self):
         self.is_animating = False
         self.image = self.sprites_idle[self.current_idle]
@@ -44,7 +56,6 @@ class Player(pygame.sprite.Sprite):
         if self.rect.y == self.pos_y:
             self.is_jumping = True
             self.gravity_force = - 15
-        
     
     def apply_gravity(self):
         self.gravity_force += 1
@@ -54,13 +65,16 @@ class Player(pygame.sprite.Sprite):
             self.rect.y = self.pos_y
             self.gravity_force = 0
             self.is_jumping = False
-   
-            
 
+    def attack(self):
+        self.is_attacking = True    
         
+    def throw_kunai(self):
+        self.is_throwing = True
 
     def update(self):
         if self.is_animating == True:
+            self.rect.x = self.pos_x
             self.current_run += 0.3
                 
             if self.current_run >= len(self.sprites_run):
@@ -78,10 +92,71 @@ class Player(pygame.sprite.Sprite):
 
             self.image = self.sprites_jump[int(self.current_jump)]
             self.image = pygame.transform.scale(self.image, (75,100))
-
         
+        if self.is_attacking == True:
+            self.current_attack += 0.3
+            
+            if self.current_attack >= len(self.sprites_attack):
+                self.current_attack = 0
+                self.is_attacking = False
+            
+            self.image = self.sprites_attack[int(self.current_attack)]
+            self.image = pygame.transform.scale(self.image, (110, 110))
+
+        if self.is_throwing == True:
+            self.current_throwing += 0.3
+
+            if self.current_throwing >= len(self.sprites_throwing):
+                self.current_throwing = 0
+                self.is_throwing = False
+
+            self.image = self.sprites_throwing[int(self.current_throwing)]
+            self.image = pygame.transform.scale(self.image, (80, 100))
+
+    def init_update(self, width, height):
+        self.rect.x = width // 2 - self.rect.width
+        self.rect.y = height - self.rect.height * 2
+        if self.is_animating == True:
+            self.current_run += 0.3
                 
+            if self.current_run >= len(self.sprites_run):
+                self.current_run = 0
+                    
+            self.image = self.sprites_run[int(self.current_run)]
+            self.image = pygame.transform.scale(self.image, (175, 200))
 
-        
-        
+            
 
+
+class Kunai:
+    def __init__(self, HEIGHT) -> None:
+        self.h = HEIGHT
+        self.angle = 0
+        self.kunai_image_original = pygame.image.load("Ninja Images/Kunai.png").convert_alpha()
+        self.kunai_image_original = pygame.transform.scale(self.kunai_image_original, (10, 50))
+        self.kunai_image = self.kunai_image_original
+        self.kunai_rect = self.kunai_image.get_rect()
+        self.kunai_rect.x = 260
+        self.kunai_rect.y = HEIGHT - 140
+        self.is_flying = False
+
+    def draw(self, screen):
+        screen.blit(self.kunai_image, self.kunai_rect)
+
+    def rotate(self):
+        self.angle -= 10  # Adjust rotation speed as necessary
+        self.kunai_image = pygame.transform.rotate(self.kunai_image_original, self.angle)
+        self.kunai_rect = self.kunai_image.get_rect(center=self.kunai_rect.center)
+
+    def fly(self):
+        if self.is_flying:
+            self.kunai_rect.x += 17
+            self.rotate()
+
+    def reset(self):
+        self.kunai_rect.x = 260
+        self.kunai_rect.y = self.h - 140
+        self.is_flying = False
+        self.angle = 0
+        self.kunai_image = self.kunai_image_original
+        self.kunai_rect = self.kunai_image.get_rect(topleft=(self.kunai_rect.x, self.kunai_rect.y))
